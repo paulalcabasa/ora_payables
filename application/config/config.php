47d6966ecffe1004fc12430cec3b3db37a7fed84
paulalcabasa <paul-alcabasa@isuzuphil.com>
2018-02-15 10:56:25 +0800

    Fix query for invoices

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://ecommerce5/ora_payables/';
+//$config['base_url'] = 'http://ecommerce5/ora_payables/';
+$config['base_url'] = 'http://'.$_SERVER['HTTP_HOST'].'/ora_payables/';
 
 /*
 |--------------------------------------------------------------------------
